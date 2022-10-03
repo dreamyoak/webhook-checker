727e7cb5e1fe203f6f730e16e849f316347fc416
jotaro <93826447+j0taro@users.noreply.github.com>
2022-10-03 17:59:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import os
+import time
 import requests
 webhook = input('webhook url >:')
 wise = '"Invalid Webhook Token"'
+haha = '"Unknown Webhook"'
 oak = webhook
 try:
  tree = requests.get(oak)
- if wise in tree.text:
-   os.system('cls')
-   print('invaild webhook fr')
+ if wise in tree.text or haha in tree.text or 'discord.com/api/webhooks' not in webhook:
+    os.system('cls')
+    print('invaild webhook fr')
+    input("Press Enter to continue...")
+    exit() 
  else:
-   os.system('cls')
-   print('vaild webhook fr')
-except:
+    os.system('cls')
+    print('vaild webhook fr')
+    input("Press Enter to continue...")
+    exit() 
+except Exception as e:
  os.system('cls')
  print('invaild webhook')
-os.system("pause")
+ input("Press Enter to continue...")
+ exit() 
